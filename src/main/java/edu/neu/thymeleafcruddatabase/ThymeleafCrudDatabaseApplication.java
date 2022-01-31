796f153669218155c325e8ceb699c8739a7d4a62
Zhiwei Bao <zhiv.bao@gmail.com>
2022-01-30 17:39:16 -0800

    Initial commit

---
+++ src/main/java/edu/neu/thymeleafcruddatabase/ThymeleafCrudDatabaseApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.thymeleafcruddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCrudDatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThymeleafCrudDatabaseApplication.class, args);
+  }
+
+}
