fe5ee62eb08209ef2a0ce9164cb3a48e13688cb8
Zhiwei Bao <zhiv.bao@gmail.com>
2022-02-05 17:13:02 -0800

    Implement "add employee"

--- src/main/java/edu/neu/thymeleafcruddatabase/controller/EmployeeController.java
+++ src/main/java/edu/neu/thymeleafcruddatabase/controller/EmployeeController.java
@@ -2,12 +2,11 @@ package edu.neu.thymeleafcruddatabase.controller;
 
 import edu.neu.thymeleafcruddatabase.entity.Employee;
 import edu.neu.thymeleafcruddatabase.service.EmployeeService;
-import java.util.ArrayList;
-import java.util.List;
-import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +22,19 @@ public class EmployeeController {
   @GetMapping("/list")
   public String listEmployees(Model model) {
     model.addAttribute("employees", employeeService.findAll());
-    return "list-employees";
+    return "employees/list-employees";
+  }
+
+  @GetMapping("/showFormForAdd")
+  public String showFormForAdd(Model model) {
+    Employee employee = new Employee();
+    model.addAttribute("employee", employee);
+    return "employees/employee-form";
+  }
+
+  @PostMapping("/save")
+  public String saveEmployee(@ModelAttribute Employee employee){
+    employeeService.save(employee);
+    return "redirect:/employees/list";
   }
 }
--- src/main/java/edu/neu/thymeleafcruddatabase/dao/EmployeeRepository.java
+++ src/main/java/edu/neu/thymeleafcruddatabase/dao/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package edu.neu.thymeleafcruddatabase.dao;
 
 import edu.neu.thymeleafcruddatabase.entity.Employee;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+  List<Employee> findAllByOrderByLastNameAsc();
 }
