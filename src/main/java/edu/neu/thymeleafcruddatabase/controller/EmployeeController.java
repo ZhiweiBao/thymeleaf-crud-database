e7d2621ca01e98d6f42805cc7f9083adc77fd63d
Zhiwei Bao <zhiv.bao@gmail.com>
2022-02-05 17:38:54 -0800

    Implement "update and delete employee"

--- src/main/java/edu/neu/thymeleafcruddatabase/controller/EmployeeController.java
+++ src/main/java/edu/neu/thymeleafcruddatabase/controller/EmployeeController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/employees")
@@ -32,9 +34,22 @@ public class EmployeeController {
     return "employees/employee-form";
   }
 
+  @GetMapping("/showFormForUpdate")
+  public String showFormForUpdate(@RequestParam("employeeId") Long employeeId, Model model) {
+    Employee employee = employeeService.findById(employeeId);
+    model.addAttribute("employee", employee);
+    return "employees/employee-form";
+  }
+
   @PostMapping("/save")
-  public String saveEmployee(@ModelAttribute Employee employee){
+  public String saveEmployee(@ModelAttribute Employee employee) {
     employeeService.save(employee);
+    return "redirect:/employees/list";
+  }
+
+  @GetMapping("/delete")
+  public String delete(@RequestParam("employeeId") Long employeeId) {
+    employeeService.delete(employeeId);
     return "redirect:/employees/list";
   }
 }
